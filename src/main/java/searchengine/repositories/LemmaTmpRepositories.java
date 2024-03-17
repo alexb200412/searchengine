21739f7ea75b019a237d261122e2d56df4091386
Alexey Barannikov <alexb2004@bk.ru>
2024-03-17 17:58:54 +0300

    исправил ошибки merge

--- src/main/java/searchengine/repositories/LemmaTmpRepositories.java
+++
@@ -1,7 +0,0 @@
-package searchengine.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import searchengine.model.LemmaTmp;
-
-public interface LemmaTmpRepositories extends JpaRepository<LemmaTmp, Integer> {
-}
